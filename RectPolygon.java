df5c0c6de4d419bcee845db65b4b10d63fb4ee29
antho <acast453@fiu.edu>
2021-10-13 16:34:58 -0700

    Loading Proj 1 to git.

---
+++ RectPolygon.java
@@ -0,0 +1,61 @@
+public class RectPolygon {
+    Point[] vertexList;
+    int size;
+    int vertNumber;
+
+    public RectPolygon() {
+        size = 100;
+        vertexList = new Point[size];
+        int Number = 0;
+
+    }
+    public void addVertex(Point v){
+
+     }
+     double area(){
+
+         return 0;
+     }
+    public Rectangle boundingRectangle(){
+
+         return null;
+     }
+     public void draw (Graphics g){
+
+     }
+    public int getNumberOfVertices(){
+
+        return 0;
+    }
+    public double greatestX(){
+
+        return 0;
+    }
+    public double greatestY(){
+
+        return 0;
+    }
+    public boolean isPointRectPolygon(Point p){
+
+        return false;
+    }
+
+    public double smallestX(){
+
+        return 0;
+    }
+
+   public double smallestY(){
+
+        return 0;
+    }
+
+    public String toString(){
+
+        return "";
+    }
+
+    public void translate(Vetor v){
+
+    }
+}
